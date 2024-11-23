1789db320d1cc39a57a674d2225f49309b8d72b6
Vlad <123636573+Coccux@users.noreply.github.com>
2024-11-23 18:56:02 +0300

    2_lab_Class_method_ValidPhone

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require_relative 'student'
 # Создание объектов студентов
-student1 = Student.new(last_name: 'Иванов', first_name: 'Иван', middle_name: 'Иванович', id: 1, phone: '123456789', email: 'ivanov@mail.com')
+student1 = Student.new(last_name: 'Иванов', first_name: 'Иван', middle_name: 'Иванович', id: 1, phone: '+7123567890', email: 'ivanov@mail.com')
 student2 = Student.new(last_name: 'Петров', first_name: 'Петр', middle_name: 'Петрович')
 student3 = Student.new(last_name: 'Сидоров', first_name: 'Сидор', middle_name: 'Сидорович', git: 'github.com/sidorov', id: '23')
 
--- student.rb
+++ student.rb
@@ -8,14 +8,26 @@ class Student
     @last_name = last_name
     @first_name = first_name
     @middle_name = middle_name
-    @phone = options[:phone]
+    self.phone = options[:phone]
     @telegram = options[:telegram]
     @email = options[:email]
     @git = options[:git]
   end
 
-
+  def self.valid_phone?(phone)
+		phone.is_a?(String) && phone.match?(/\A\+7\d{10}\z/)
+  end 
   
+  
+  def phone=(phone)
+	if phone.nil? || Student.valid_phone?(phone)
+		@phone = phone
+	else
+		raise ArgumentError, "Некорректный номер телефона: #{phone}"
+	end
+  end
+
+
   # Метод для вывода информации о студенте
   def to_s
     "ID: #{id || 'не указан'}, ФИО: #{last_name} #{first_name} #{middle_name}, Телефон: #{phone || 'не указан'}, Telegram: #{telegram || 'не указан'}, Email: #{email || 'не указан'}, Git: #{git || 'не указан'}"
