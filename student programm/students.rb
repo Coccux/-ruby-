95a6a33aafcd349ab7859b33742172d64361182e
Vlad <123636573+Coccux@users.noreply.github.com>
2025-01-21 11:01:07 +0300

    2lab_task1.3,4_add_attr_accessor

--- student programm/students.rb
+++ student programm/students.rb
@@ -1,5 +1,6 @@
 class Student
-  
+	attr_accessor :id, :surname, :name, :patronymic, :nomber_phone, :telegram, :email, :git
+	
 	def initialize(id:nil, surname:, name: , patronymic:,number_phone:nil, telegram: nil, email: nil, git:nil)
 		@id = id
 		@surname = surname
@@ -11,59 +12,6 @@ class Student
 		@git = git
 	end
 
-	
-	
-	def set_id=(new_id)
-		@id=new_id
-	end
-	def set_surname=(new_surname)
-		@surname=new_surname
-	end
-	def set_name=(new_name)
-		@name=new_name
-	end
-	def set_patronymic=(new_patronymic)
-		@patronymic=new_patronymic
-	end
-	def set_number_phone=(new_number_phone)
-		@number_phone=new_number_phone
-	end
-	def set_telegram=(new_telegram)
-		@telegram=new_telegram
-	end
-	def set_email=(new_email)
-		@email=new_email
-	end
-	def set_git=(new_git)
-		@git=new_git
-	end
-	
-	
-	
-	def get_id
-		@id
-	end
-	def get_surname
-		@surname
-	end
-	def get_name
-		@name
-	end
-	def get_patronymic
-		@patronymic
-	end
-	def get_number_phone
-		@number_phone
-	end
-	def get_telegram
-		@telegram
-	end
-	def get_email
-		@email
-	end
-	def get_git
-		@git
-	end  
 	
 	def print_info
 		puts ("\nID студента: #{@id}\nФИО: #{@surname} #{@name} #{@patronymic}")
