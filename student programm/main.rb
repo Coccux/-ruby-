f10f091a213ad6bd682c7cb074da1ffaef56b0fb
Vlad <123636573+Coccux@users.noreply.github.com>
2025-01-21 11:18:33 +0300

    2lab_task1.5,6_arguments_as_hash

--- student programm/main.rb
+++ student programm/main.rb
@@ -1,10 +1,10 @@
 require_relative 'students'
 
 stud=Student.new(
-	id: "1",
 	surname: "Иванов",
 	name:"Иван",
 	patronymic: "Иванович",
+	id: "1",
 	number_phone: "81234567890",
 	telegram: "ssss",
 	email: "dddd",
--- student programm/students.rb
+++ student programm/students.rb
@@ -1,23 +1,24 @@
 class Student
 	attr_accessor :id, :surname, :name, :patronymic, :nomber_phone, :telegram, :email, :git
 	
-	def initialize(id:nil, surname:, name: , patronymic:,number_phone:nil, telegram: nil, email: nil, git:nil)
-		@id = id
-		@surname = surname
-		@name = name
-		@patronymic = patronymic
-		@number_phone = number_phone
-		@telegram = telegram
-		@email = email
-		@git = git
+	def initialize(attributes = {})
+		@id = attributes[:id]
+		@surname = attributes[:surname]
+		@name = attributes[:name]
+		@patronymic = attributes[:patronymic]
+		@number_phone = attributes[:number_phone]
+		@telegram = attributes[:telegram]
+		@email = attributes[:email]
+		@git = attributes[:git]
 	end
 
 	
 	def print_info
-		puts ("\nID студента: #{@id}\nФИО: #{@surname} #{@name} #{@patronymic}")
-		puts ("Номер телефон: #{@number_phone}") if @number_phone  
-		puts ("Телеграмм: #{@telegram}") if @telegram 
-		puts ("Почта: #{@email}") if @email 
-		puts ("Гит: #{@git}") if @git 
+		puts "\nID студента: #{@id}" if @id
+		puts "ФИО: #{@surname} #{@name} #{@patronymic}" if @surname && @name && @patronymic
+		puts "Номер телефон: #{@number_phone}" if @number_phone  
+		puts "Телеграмм: #{@telegram}" if @telegram 
+		puts "Почта: #{@email}" if @email 
+		puts "Гит: #{@git}" if @git 
   end
 end
