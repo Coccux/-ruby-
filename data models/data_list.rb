5d194dd7001e2f47b2d3de4a802da8798d144c1e
Vlad <123636573+Coccux@users.noreply.github.com>
2025-02-06 12:52:06 +0300

    4lab_task1.9

--- data models/data_list.rb
+++ data models/data_list.rb
@@ -1,49 +1,57 @@
-require_relative 'deep_copy.rb'
-require_relative 'data_table.rb'
-class DataList
-	def initialize(items)
-		raise ArgumentError, 'Items must be an array' unless items.is_a?(Array)
-		@items = items
-		@selected = []
-	end
-	def select(number)
-		raise ArgumentError, 'Invalid index' unless  number >= 0 && number < @items.size
-		@selected << number unless @selected.include?(number)
-	end
-	def get_selected
-		DeepCopy.copy(selected)
-	end
-  
-	def proceed_data_table
-		dt=[]
-		dt << self.get_names
-		dt.concat(self.get_data)
-		DataTable.new(dt)
-	end
-	def get_names
-		column_names = ['№'] 
+class Data_list
+
+    def initialize(data)
+        self.data = data
+        self.selected = []
+    end
+
+    
+    def select(number)
+        unless number.is_a?(Integer) && number >= 0 && number < data.size
+            raise ArgumentError, 'Параметр должен быть числом в пределах размера массива'
+        end
+        self.selected << number unless self.selected.include?(number)
+    end
+
+    def get_selected
+        self.selected
+    end
+
+    def get_names
+        column_names = ['№'] 
         base_names.each do |name| 
             column_names << name
         end
         column_names
-	end
-	def get_data
-		data_table = [get_names]  
+    end
+
+    def get_data
+        data_table = [get_names] 
         data.each_with_index do |item, index|
             data_table << build_row(item, index)
         end
         Data_table.new(data_table)
-	end
-	
-	def base_names
+    end
+
+    def data=(data)
+        
+        unless data.is_a?(Array)
+            raise ArgumentError, 'Переданный параметр должен быть массивом'
+        end
+
+        @data = data
+    end
+
+    protected
+    
+    attr_reader :data
+    attr_accessor :selected
+
+    def base_names
         raise NotImplementedError, 'Метод не реализован в данном классе'
     end    
-	
-    
+
     def build_row(item, index)
         raise NotImplementedError, 'Метод не реализован в данном классе'
-    end
-	
-	protected
-	attr_accessor :selected
+    end    
 end
