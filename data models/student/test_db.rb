8b7dc5017714fb37184d886457867891025d5430
Vlad <123636573+Coccux@users.noreply.github.com>
2025-02-07 18:11:46 +0300

    4lab_task3.8_The Students_list_db class has been implemented to work with the student database

--- data models/student/test_db.rb
+++ data models/student/test_db.rb
@@ -1,27 +1,25 @@
 require 'dotenv/load'
 require 'pg'
 require './models/student/student.rb'
+require './models/students_list/students_list_db.rb'
+
 host = ENV['DB_HOST']
 port = ENV['DB_PORT']
 dbname = ENV['DB_NAME']
 user = ENV['DB_USER']
 password = ENV['DB_PASSWORD']
 
-begin
-    connection = PG.connect(
-        host: host,
-        port: port,
-        dbname: dbname,
-        user: user,
-        password: password
-    )
+db_params = {host: host, port: port, dbname: dbname, user: user, password: password}
 
-    result = connection.exec("SELECT * FROM student;")
+students_list_db = Students_list_db.new(db_params)
 
-    result.each do |row|
-        puts Student.new_from_hash(row).initials
-    end
-ensure
+test_student = Student.new(surname:"Сидоров", firstname:"Сидор", lastname:"Сидорович", phone_number: "89182297016", date_of_birth: '1990-02-02')
+# students_list_db.add_student(test_student)
 
-    connection.close if connection
+data_list = students_list_db.get_k_n_student_short_list(1, 50)
+data = data_list.get_data
+(0..data.row_count - 1).each do |index|
+    puts "#{data.get_element(index, 0)}, #{data.get_element(index, 1)}, #{data.get_element(index, 2)}, #{data.get_element(index, 3)}"   
 end
+
+puts "Количество записей в таблице: #{students_list_db.get_student_short_count}"
