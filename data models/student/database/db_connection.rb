77bd23d1bdf14839ad1449c5b8e976dc4b0c9e3f
Vlad <123636573+Coccux@users.noreply.github.com>
2025-02-07 21:09:44 +0300

    4lab_task3.11_The Singleton pattern for DB_connection has been applied

--- data models/student/database/db_connection.rb
+++ data models/student/database/db_connection.rb
@@ -1,14 +1,29 @@
 require 'pg'
+
 class DB_connection
-    def initialize(db_config)
-        self.connection = PG.connect(db_config)
+    private_class_method :new
+
+    def self.instance(db_config = nil)
+        @instance ||= new(db_config)
     end
+
     def query(query, params=[])
         self.connection.exec_params(query, params)
     end
+
     def close
         self.connection.close
+        self.connection.nil
+        @instance = nil
     end
+
     private
     attr_accessor :connection
+
+    @instance = nil
+
+    def initialize(db_config)
+        raise "Отсутсвует конфигурация БД" unless db_config
+        self.connection = PG.connect(db_config)
+    end
 end
